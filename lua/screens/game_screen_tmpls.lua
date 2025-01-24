f84e1c5e5eb52670e028913f9e93c8b6776e9c73
diogoDSN <72698868+diogoDSN@users.noreply.github.com>
2025-01-24 22:12:22 +0000

    Added screen template

---
+++ lua/screens/game_screen_tmpls.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.load = function ()
+	print "This screen has no load function."
+end
+
+M.draw = function ()
+	print "This screen has no draw function."
+end
+
+-- Update functions should return a new screen in case we change screen
+M.update = function (dt)
+	print "This screen has no update function."
+	return nil
+end
+
+return M
--- lua/state.lua
+++ lua/state.lua
@@ -1,19 +1,7 @@
 local game_screen = require("lua.screens.game_screen")
+
 local M = {}
 
-local noScreen = {
-	load = function ()
-		print "This screen has no load function."
-	end,
-	draw = function ()
-		print "This screen has no draw function."
-	end,
-	-- Update functions should return a new screen in case we change screen
-	update = function (dt)
-		print "This screen has no update function."
-		return nil
-	end
-}
-
 M.screen = game_screen
+
 return M
