56b9c5c39c5b72cf91913656f1ce3c876b35aca2
Rodrigo Duarte <128046980+Rodrigo-Duarte-8128@users.noreply.github.com>
2025-01-24 21:34:58 +0000

    rd | change load and state order (#10)
    Co-authored-by: Rodrigo-Duarte-8128 <rodrigo.duarte@freiheit.com>

--- lua/draw.lua
+++ lua/draw.lua
@@ -1,4 +1,4 @@
-require "lua.state"
+local state = require("lua.state")
 
 function love.draw()
     local width = love.graphics.getWidth()
@@ -10,7 +10,7 @@ function love.draw()
 
     love.graphics.setColor(1, 1, 1)
     love.graphics.setLineWidth(4)
-    love.graphics.circle("line", Bubble.center_x, Bubble.center_y, Bubble.outer_radius)
+    love.graphics.circle("line", state.bubble.center_x, state.bubble.center_y, state.bubble.outer_radius)
     love.graphics.setColor(0.2, 0.2, 1, 0.5)
-    love.graphics.circle("fill", Bubble.center_x, Bubble.center_y, Bubble.inner_radius)
+    love.graphics.circle("fill", state.bubble.center_x, state.bubble.center_y, state.bubble.inner_radius)
 end
