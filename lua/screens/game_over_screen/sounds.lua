652c500d5ac62510214e47449532f7837c501787
Luis Ferreira <luis.pmm.ferreira@gmail.com>
2025-01-25 14:27:51 +0000

    sutff

--- lua/screens/game_over_screen/sounds.lua
+++ lua/screens/game_over_screen/sounds.lua
@@ -2,5 +2,6 @@ local M = {}
 
 M.game_over = love.audio.newSource("archive/sounds/game_over_1.wav", "static")
 M.game_over_deep = love.audio.newSource("archive/sounds/game_over_deep.wav", "static")
+M.pop_cut = love.audio.newSource("archive/sounds/pop_cut.wav", "static")
 
 return M
--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ require "lua.draw"
 require "lua.update"
 local state = require "lua.state"
 
+math.randomseed(os.time())
+
 function love.load()
     love.window.setMode(1024, 1024)
     state.current_screen.load()
