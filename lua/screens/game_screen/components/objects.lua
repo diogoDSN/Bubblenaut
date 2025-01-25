2693d3a9aef14d43c4db0e7039c612b5a8cb3631
Miguel Fernandes <miguelfernandesfernandes10@gmail.com>
2025-01-25 00:03:00 +0000

    Add spike (#21)

--- lua/screens/game_screen/components/objects.lua
+++ lua/screens/game_screen/components/objects.lua
@@ -8,4 +8,14 @@ M.bubble = {
     step = 50,
 }
 
+M.spike = {
+    center_x = 800,
+    center_y = 200,
+    inner_radius = 50,
+    outer_radius = 52,
+    step = 50,
+    fill_color = {1, 0, 0, 0.5},
+    line_color = {1, 0, 0},
+}
+
 return M
