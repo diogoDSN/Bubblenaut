9b1cbe82b6943ab9a1e9f4762c60d7c40a209e56
Miguel Fernandes <miguel.fernandes@freiheit.com>
2025-01-25 15:49:01 +0000

    Add game width and height to config

--- main.lua
+++ main.lua
@@ -2,12 +2,12 @@ require "lua.draw"
 require "lua.update"
 local state = require "lua.state"
 local push = require "push"
+local conf = require "conf"
 
-local gameWidth, gameHeight = 540, 540 --fixed game resolution
 local windowWidth, windowHeight = love.window.getDesktopDimensions()
 windowWidth, windowHeight = windowHeight*.9, windowHeight*.9 --make the window a bit smaller than the screen itself
 
-push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = false})
+push:setupScreen(conf.gameWidth, conf.gameHeight, windowWidth, windowHeight, {fullscreen = false})
 
 math.randomseed(os.time())
 
