c29ce8843594f85c7ee4390ff6906e4342f5e424
Rodrigo Duarte <128046980+Rodrigo-Duarte-8128@users.noreply.github.com>
2025-01-25 14:44:47 +0000

    rd | improve controls and refactor movements (#36)
    Co-authored-by: Rodrigo-Duarte-8128 <rodrigo.duarte@freiheit.com>

--- lua/screens/screen_template.lua
+++ lua/screens/screen_template.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-M.load = function ()
-	print "This screen has no load function."
+M.load = function()
+    print "This screen has no load function."
 end
 
-M.draw = function ()
-	print "This screen has no draw function."
+M.draw = function()
+    print "This screen has no draw function."
 end
 
 -- Update functions should return a new screen in case we change screen
-M.update = function (dt)
-	print "This screen has no update function."
-	return nil
+M.update = function(dt)
+    print "This screen has no update function."
+    return nil
 end
 
 return M
