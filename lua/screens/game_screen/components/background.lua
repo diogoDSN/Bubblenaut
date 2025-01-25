a24b3f55c4e31a7db51a81250fe0c7d653b2a0df
Miguel Fernandes <miguelfernandesfernandes10@gmail.com>
2025-01-25 16:20:26 +0000

    Update game-background (#45)

--- lua/screens/game_screen/components/background.lua
+++ lua/screens/game_screen/components/background.lua
@@ -1,16 +1,16 @@
 local M = {}
+local conf = require("conf")
 
 local SCROLL_SPEED = 5
 
 M.setup_background = function ()
     M.game_background_image = love.graphics.newImage("archive/game-background.png")
 	M.game_background_image:setWrap("clamp", "repeat")
-	M.game_background_scale_factor = love.graphics.getWidth() / M.game_background_image:getWidth()
-
+	M.game_background_scale_factor = conf.gameWidth / M.game_background_image:getWidth()
 
 	M.screenQuad = love.graphics.newQuad(
-		0, -M.game_background_image:getHeight() + love.graphics.getHeight(),
-		love.graphics.getWidth(), love.graphics.getHeight(),
+		0, -M.game_background_image:getHeight() + conf.gameHeight,
+		conf.gameWidth, conf.gameHeight,
 		M.game_background_image
 	)
 	M.y = 0
@@ -20,7 +20,7 @@ M.update_background = function ()
 	M.y = (M.y - SCROLL_SPEED) % M.game_background_image:getHeight()
 	M.screenQuad:setViewport(
 		0, M.y,
-		love.graphics.getWidth(), love.graphics.getHeight(),
+		conf.gameWidth, conf.gameHeight,
 		M.game_background_image:getWidth(), M.game_background_image:getHeight()
 	)
 end
