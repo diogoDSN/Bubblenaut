5e6e23fcb3af78b3dd710ebe0cc6646aa8139339
diogoDSN <72698868+diogoDSN@users.noreply.github.com>
2025-01-25 16:05:28 +0000

    Background now scrolls

--- lua/screens/game_screen/init_screen.lua
+++ lua/screens/game_screen/init_screen.lua
@@ -8,11 +8,13 @@ local M = {}
 -- runs once when opening the game screen
 M.load = function()
 	objects.setupGame()
+	background.setup_background()
 end
 
 -- function to run when love updates the game state, runs before drawing
 M.update = function(dt)
     movement.handle_movement(dt)
+	background.update_background()
 end
 
 -- function to run when love draws the game screen
@@ -24,8 +26,7 @@ M.draw = function()
     love.graphics.print("A Bubble", width / 10, height / 10)
 
     -- Background
-    --local game_background_image = love.graphics.newImage("archive/game-background.png")
-    --love.graphics.draw(game_background_image, background.background.center_x, background.background.center_y, 0, 1, 1, background.background.width / 2, background.background.height)
+	background.draw_backgroud()
 
     -- Bubble
     objects.draw_bubble()
