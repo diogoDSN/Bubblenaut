25b84ef26711d7551816ec72b865c78c058a8bae
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2025-01-25 16:03:20 +0000

    Transition pages with a beamer (#39)
    This enables screen switches with timing associated with it, such that
    it is possible to play animations and sounds during that transition.

--- lua/screens/menu_screen/init_screen.lua
+++ lua/screens/menu_screen/init_screen.lua
@@ -1,15 +1,22 @@
-local love = require("love")
-local M = {}
+local router = require("lua.commons.router")
 local conf = require "conf"
 
-M.load = function ()
+local M = {}
+
+
+local beamer = router.new_beamer(
+    "game_screen",
+    0.2
+)
+
+M.load = function()
     print("Menu screen loaded")
     M.background = love.graphics.newImage("archive/menu-background.png")
 end
 
-M.draw = function ()
+M.draw = function()
     love.graphics.draw(M.background, 0, 0)
-    love.graphics.setColor(1, 1, 1, 1) 
+    love.graphics.setColor(1, 1, 1, 1)
     love.graphics.setFont(love.graphics.newFont(40))
     love.graphics.printf("Welcome to", 0, conf.gameHeight / 8, conf.gameWidth, "center")
     love.graphics.printf("Bubblenaut", 0, conf.gameHeight / 4, conf.gameWidth, "center")
@@ -17,11 +24,17 @@ M.draw = function ()
 end
 
 
-M.update = function ()
+M.update = function(dt)
     if love.keyboard.isDown("space") then
-        return "game_screen"
+        beamer:activate()
     end
+
+    local next_screen = beamer:update(dt)
+    if next_screen ~= nil then
+        return next_screen
+    end
+
+    return nil
 end
 
 return M
-
