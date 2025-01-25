4984e33fed41abfb70cc377da96904d7b57db0dd
Margareta Schlueter <dizrc750@dentsplysirona.com>
2025-01-25 21:52:26 +0000

    level 1

--- lua/screens/game_screen/components/objects.lua
+++ lua/screens/game_screen/components/objects.lua
@@ -116,13 +116,60 @@ M.setupGame = function()
     )
 
     M.obstacles = {
-       -- {x, y} coordinates relative to the whole level
-       {1 * M.spike_radius, -M.spike_radius},
-       {13 * M.spike_radius, -5 * M.spike_radius},
-       {7 * M.spike_radius, -10 * M.spike_radius}
+       -- {x, y} coordinates in a grid of width 14, obstacles are 2 columns wide
+       -- x is a number between 1 and 13, the obstacle will be in columns x and x+1
+       -- y is a negative number
+       {2, 6},
+       {10, 0},
+       {12, 0},
+       {2, -8},
+       {4, -8},
+       {6, -8},
+       {1, -20},
+       {13, -20},
+       {3, -22},
+       {11, -22},
+       {5, -24},
+       {9, -24},
+       {7, -34},
+       {5, -36},
+       {7, -36},
+       {9, -36},
+       {7, -38},
+       {1, -46},
+       {3, -46},
+       {5, -46},
+       {7, -46},
+       {4, -54},
+       {7, -54},
+       {10, -54},
+       {13, -54},
+       {1, -64},
+       {5, -64},
+       {9, -64},
+       {13, -64},
+       {3, -68},
+       {7, -68},
+       {11, -68},
+       {6, -74},
+       {12, -77},
+       {2, -78},
+       {9, -80},
+       {5, -82},
+       {12, -84},
+       {10, -85},
+       {3, -87},
+       {7, -88},
+       {1, -90},
+       {12, -90}
     }
+
+    for _, spike in ipairs(M.obstacles) do
+        spike[1] = spike[1] * M.spike_radius
+        spike[2] = spike[2] * M.spike_radius
+    end
        
-    M.finish_line = -20 * M.spike_radius
+    M.finish_line = -95 * M.spike_radius
     M.game_state = ""
 end
 
