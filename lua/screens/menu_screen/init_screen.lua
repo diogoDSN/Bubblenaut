057af9a2d49d678835bd6eb3e96195f0387e7a2b
Miguel Fernandes <miguelfernandesfernandes10@gmail.com>
2025-01-25 15:51:37 +0000

    Add game width and height to config (#38)

--- lua/screens/menu_screen/init_screen.lua
+++ lua/screens/menu_screen/init_screen.lua
@@ -1,28 +1,26 @@
+local love = require("love")
 local M = {}
+local conf = require "conf"
 
 M.load = function ()
     print("Menu screen loaded")
+    M.background = love.graphics.newImage("archive/menu-background.png")
 end
 
 M.draw = function ()
-    local background = love.graphics.newImage("archive/menu-background.png")
-    M.draw = function ()
-        love.graphics.draw(background, 0, 0)
-        love.graphics.setColor(1, 1, 1, 1) 
-        love.graphics.setFont(love.graphics.newFont(40))
-        love.graphics.printf("Welcome to", 0, love.graphics.getHeight() / 8, love.graphics.getWidth(), "center")
-        love.graphics.printf("Bubblenaut", 0, love.graphics.getHeight() / 4, love.graphics.getWidth(), "center")
-        love.graphics.printf("Press SPACE to Start :)", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
-    end
+    love.graphics.draw(M.background, 0, 0)
+    love.graphics.setColor(1, 1, 1, 1) 
+    love.graphics.setFont(love.graphics.newFont(40))
+    love.graphics.printf("Welcome to", 0, conf.gameHeight / 8, conf.gameWidth, "center")
+    love.graphics.printf("Bubblenaut", 0, conf.gameHeight / 4, conf.gameWidth, "center")
+    love.graphics.printf("Press SPACE to Start :)", 0, conf.gameHeight / 2, conf.gameWidth, "center")
 end
 
 
-M.update = function (dt)
+M.update = function ()
     if love.keyboard.isDown("space") then
         return "game_screen"
     end
-
-	return nil
 end
 
 return M
