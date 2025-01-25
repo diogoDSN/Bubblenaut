851d811513a2d3d9292f5a98759db13eabd472bb
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2025-01-25 17:02:05 +0000

    Get rid of inner radius (#49)

--- lua/screens/game_screen/components/objects.lua
+++ lua/screens/game_screen/components/objects.lua
@@ -6,24 +6,17 @@ local M = {}
 M.bubble = {
     center_x = 0,
     center_y = 0,
-    inner_radius = 0,
-    outer_radius = 0,
+    radius = 0,
     step = 0,
 }
-
-function M.bubble:get_outer_line_width()
-    return 2 * (self.outer_radius - self.inner_radius)
-end
 
 function M.bubble.grow(self)
     local expansion_factor = configs.sizes.expansion_factor
     local step_increase_factor = configs.steps.step_increase_factor
 
-    local new_inner_radius = self.inner_radius * expansion_factor
-    local new_outer_radius = self.outer_radius * expansion_factor
+    local new_radius = self.radius * expansion_factor
 
-    self.inner_radius = new_inner_radius
-    self.outer_radius = new_outer_radius
+    self.radius = new_radius
 
     self.step = self.step * step_increase_factor
 end
@@ -32,11 +25,9 @@ function M.bubble.shrink(self)
     local shrink_factor = configs.sizes.shrink_factor
     local step_reduction_factor = configs.steps.step_reduction_factor
 
-    local new_inner_radius = self.inner_radius * shrink_factor
-    local new_outer_radius = self.outer_radius * shrink_factor
+    local new_radius = self.radius * shrink_factor
 
-    self.inner_radius = new_inner_radius
-    self.outer_radius = new_outer_radius
+    self.radius = new_radius
 
     self.step = self.step * step_reduction_factor
 end
@@ -54,14 +45,14 @@ M.draw_bubble = function()
     local center_x = bubble_sprite_width / 2
     local center_y = bubble_sprite_height / 2
 
-    local scale_factor = (M.bubble.outer_radius / bubble_sprite_width) * 2
+    local scale_factor = (M.bubble.radius / bubble_sprite_width) * 2
 
     love.graphics.draw(
-        M.bubble.sprite,                -- sprite
+        M.bubble.sprite,                      -- sprite
         M.bubble.center_x, M.bubble.center_y, -- position
-        0,                              -- rotation
-        scale_factor, scale_factor,     -- scaling
-        center_x, center_y              -- pivot
+        0,                                    -- rotation
+        scale_factor, scale_factor,           -- scaling
+        center_x, center_y                    -- pivot
     )
 end
 
@@ -74,7 +65,7 @@ local spike_pivot_y = spike_sprite:getHeight() / 2
 M.draw_obstacles = function()
     for _, spike in ipairs(M.obstacles) do
         spike_center_x = spike[1]
-        spike_center_y = M.bubble.center_y - (spike[2] - M.y_position) 
+        spike_center_y = M.bubble.center_y - (spike[2] - M.y_position)
 
         love.graphics.draw(
             spike_sprite,                   -- sprite
@@ -93,15 +84,14 @@ M.setupGame = function()
 
     M.bubble.center_x = conf.gameWidth / 2
     M.bubble.center_y = conf.gameHeight / 2 + bubble_y_offset
-    M.bubble.inner_radius = 50
-    M.bubble.outer_radius = 52
+    M.bubble.radius = 52
     M.bubble.step = 50
 
     M.obstacles = {
         -- {x, y} coordinates relative to the whole level
-	    {200, 500},
-	    {800, 1000},
-	    {400, 1500}
+        { 200, 500 },
+        { 800, 1000 },
+        { 400, 1500 }
     }
 
     M.y_position = 0
