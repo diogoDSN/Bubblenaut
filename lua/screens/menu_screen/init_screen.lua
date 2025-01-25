f5a5e7b28d6e8329a9e61ff6cd73d77cde9a683e
Luis Ferreira <luis.pmm.ferreira@gmail.com>
2025-01-25 15:58:08 +0000

    beamer on main transition

--- lua/screens/menu_screen/init_screen.lua
+++ lua/screens/menu_screen/init_screen.lua
@@ -1,29 +1,41 @@
+local router = require("lua.commons.router")
+
 local M = {}
 
-M.load = function ()
+M.load = function()
     print("Menu screen loaded")
 end
 
-M.draw = function ()
+M.draw = function()
     local background = love.graphics.newImage("archive/menu-background.png")
-    M.draw = function ()
+    M.draw = function()
         love.graphics.draw(background, 0, 0)
-        love.graphics.setColor(1, 1, 1, 1) 
+        love.graphics.setColor(1, 1, 1, 1)
         love.graphics.setFont(love.graphics.newFont(40))
         love.graphics.printf("Welcome to", 0, love.graphics.getHeight() / 8, love.graphics.getWidth(), "center")
         love.graphics.printf("Bubblenaut", 0, love.graphics.getHeight() / 4, love.graphics.getWidth(), "center")
-        love.graphics.printf("Press SPACE to Start :)", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+        love.graphics.printf("Press SPACE to Start :)", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(),
+            "center")
     end
 end
 
 
-M.update = function (dt)
+local beamer = router.new_beamer(
+    "game_screen",
+    0.2
+)
+
+M.update = function(dt)
     if love.keyboard.isDown("space") then
-        return "game_screen"
+        beamer:activate()
     end
 
-	return nil
+    local next_screen = beamer:update(dt)
+    if next_screen ~= nil then
+        return next_screen
+    end
+
+    return nil
 end
 
 return M
-
