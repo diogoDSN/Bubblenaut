7d4f83f23a3226c7b83afcc3a224a96d7cd7ef92
Luis Ferreira <luis.pmm.ferreira@gmail.com>
2025-01-25 14:45:58 +0000

    animations util split from screen logic

--- lua/screens/game_over_screen/init_screen.lua
+++ lua/screens/game_over_screen/init_screen.lua
@@ -1,73 +1,7 @@
 local sounds = require("lua.screens.game_over_screen.sounds")
+local animations = require("lua.commons.animations")
 
 local M = {}
-
-
----@param sound_track love.Source # sound source to play with the animation (beware: sound_track pitch will be adapted based on desired duration)
-local new_animation = function(
-    sprite_sheet,
-    width,
-    height,
-    x,
-    y,
-    duration,
-    pre_start,
-    repeatable,
-    sound_track
-)
-    local animation = {}
-    animation.sprite_sheet = sprite_sheet;
-    animation.quads = {};
-    animation.quad_n = 0;
-
-    for y = 0, sprite_sheet:getHeight() - height, height do
-        for x = 0, sprite_sheet:getWidth() - width, width do
-            table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, sprite_sheet:getDimensions()))
-            animation.quad_n = animation.quad_n + 1;
-        end
-    end
-
-    animation.duration = duration or 1
-    animation.current_time = 0
-    animation.played = false
-    animation.started = pre_start
-    animation.repeatable = repeatable
-
-    local sound_duration = sound_track:getDuration("seconds")
-    sound_track:setPitch(sound_duration / duration)
-    animation.sound_track = sound_track
-
-    function animation.draw(self)
-        local sprite_n = math.floor(self.current_time / self.duration * 3) + 1
-
-        if sprite_n > self.quad_n then
-            sprite_n = self.quad_n
-        end
-
-        love.graphics.draw(self.sprite_sheet, self.quads[sprite_n], x, y, 0, 1)
-    end
-
-    function animation.update(self, dt)
-        if not self.started then
-            return
-        end
-        self.current_time = self.current_time + dt
-        if self.current_time >= self.duration and self.repeatable then
-            self.current_time = self.current_time - self.duration
-        end
-
-        if not self.sound_track:isPlaying() and not self.played then
-            love.audio.play(self.sound_track)
-            self.played = true
-        end
-    end
-
-    function animation.start(self)
-        self.started = true
-    end
-
-    return animation
-end
 
 local game_over_sound = {
     countdown = 0.2,
@@ -81,7 +15,7 @@ local beamer = {
     destination = "game_screen"
 }
 
-local pop_animation = new_animation(
+local pop_animation = animations.new_animation(
     love.graphics.newImage("archive/bubble_pop.png"),
     128,
     128,
