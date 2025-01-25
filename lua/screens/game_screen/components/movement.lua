c0c87b1e4e54af6132dbd5c9ebe94da521070069
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2025-01-25 00:31:31 +0000

    Add the wav recordings and load inflate/deflate sound (#26)
    Initial draft on including the sounds onto the game

--- lua/screens/game_screen/components/movement.lua
+++ lua/screens/game_screen/components/movement.lua
@@ -1,5 +1,6 @@
 local configs = require("lua.screens.game_screen.config")
 local objects = require("lua.screens.game_screen.components.objects")
+local sounds = require("lua.screens.game_screen.components.sounds")
 local background = require("lua.screens.game_screen.components.background")
 local graphics = require("lua.screens.game_screen.components.graphics")
 
@@ -11,6 +12,8 @@ local right_movement_enabled = true
 local positions_list = {}
 
 local scroll_speed = 100 -- pixels per second
+
+local debug_prints = true
 
 function love.keypressed(key)
     local left_wall = 0
@@ -53,7 +56,21 @@ function love.keypressed(key)
     end
 end
 
+local function clamp(v, min, max)
+    if v > max then
+        return max
+    end
+    if v < min then
+        return min
+    end
+    return v
+end
+
 M.handle_movement = function(dt)
+    if debug_prints then
+        print(1 / dt, "fps")
+    end
+
     local expansion_factor = configs.sizes.expansion_factor
     local shrink_factor = configs.sizes.shrink_factor
 
@@ -78,14 +95,16 @@ M.handle_movement = function(dt)
         local new_inner_radius = objects.bubble.inner_radius * expansion_factor
         local new_outer_radius = objects.bubble.outer_radius * expansion_factor
 
+        objects.bubble.step = objects.bubble.step * step_increase_factor
+
         local new_bubble_max_x = objects.bubble.center_x + new_outer_radius +
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
         local new_bubble_min_x = objects.bubble.center_x - new_outer_radius -
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
         local new_bubble_max_y = objects.bubble.center_y + new_outer_radius +
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
         local new_bubble_min_y = objects.bubble.center_y - new_outer_radius -
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
 
         if new_bubble_max_x <= max_x and new_bubble_min_x >= min_x and new_bubble_max_y <= max_y and new_bubble_min_y >= min_y then
             objects.bubble.inner_radius = new_inner_radius
@@ -93,19 +112,31 @@ M.handle_movement = function(dt)
 
             objects.bubble.step = objects.bubble.step * step_increase_factor
         end
+
+        if not sounds.inflating:isPlaying() then
+            love.audio.play(sounds.inflating)
+        end
+    else
+        if sounds.inflating:isPlaying() then
+            love.audio.pause(sounds.inflating)
+            sounds.inflating.seek(sounds.inflating, 0, "seconds")
+        end
     end
+
     if love.keyboard.isDown(configs.controls.shrink_key) then
         local new_inner_radius = objects.bubble.inner_radius * shrink_factor
         local new_outer_radius = objects.bubble.outer_radius * shrink_factor
 
+        objects.bubble.step = objects.bubble.step * step_reduction_factor
+
         local new_bubble_max_x = objects.bubble.center_x + new_outer_radius +
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
         local new_bubble_min_x = objects.bubble.center_x - new_outer_radius -
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
         local new_bubble_max_y = objects.bubble.center_y + new_outer_radius +
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
         local new_bubble_min_y = objects.bubble.center_y - new_outer_radius -
-        graphics.bubble_outer_line_width(objects.bubble) / 2
+            graphics.bubble_outer_line_width(objects.bubble) / 2
 
         if new_bubble_max_x <= max_x and new_bubble_min_x >= min_x and new_bubble_max_y <= max_y and new_bubble_min_y >= min_y then
             objects.bubble.inner_radius = new_inner_radius
@@ -113,7 +144,34 @@ M.handle_movement = function(dt)
 
             objects.bubble.step = objects.bubble.step * step_reduction_factor
         end
+
+        if not sounds.deflating:isPlaying() then
+            love.audio.play(sounds.deflating)
+        end
+    else
+        if sounds.deflating:isPlaying() then
+            love.audio.pause(sounds.deflating)
+            sounds.inflating.seek(sounds.deflating, 0, "seconds")
+        end
     end
+
+    objects.bubble.inner_radius = clamp(
+        objects.bubble.inner_radius,
+        configs.sizes.min_inner_radius,
+        configs.sizes.max_inner_radius
+    )
+
+    objects.bubble.outer_radius = clamp(
+        objects.bubble.outer_radius,
+        configs.sizes.min_outer_radius,
+        configs.sizes.max_outer_radius
+    )
+
+    objects.bubble.step = clamp(
+        objects.bubble.step,
+        configs.steps.min_step,
+        configs.steps.max_step
+    )
 
     objects.spike.center_y = objects.spike.center_y + scroll_speed * dt
 
--- lua/screens/game_screen/config.lua
+++ lua/screens/game_screen/config.lua
@@ -11,6 +11,8 @@ M.steps = {
     bubble_step = 50,
     step_increase_factor = 1.1,
     step_reduction_factor = 0.9,
+    max_step = 200,
+    min_step = 10,
     animation_step = 10,
 }
 
@@ -19,6 +21,10 @@ M.sizes = {
     initial_outer_radius = 52,
     expansion_factor = 1.1,
     shrink_factor = 0.9,
+    max_inner_radius = 200,
+    max_outer_radius = 208,
+    min_inner_radius = 10,
+    min_outer_radius = 10.4,
 }
 
 return M
